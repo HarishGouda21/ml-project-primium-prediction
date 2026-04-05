6c894acc53f7680c62bc104c158c17b97c2085b5
Harish Gouda <your_email@gmail.com>
2026-04-05 08:58:28 +0530

    fixed requirements and paths

--- prediction_helper.py
+++ prediction_helper.py
@@ -3,12 +3,12 @@ from joblib import load
 
 # from app.main import insurance_plan
 
-model_rest = load("artifacts\model_rest.joblib")
-model_young = load("artifacts\model_young.joblib")
+model_rest = load("artifacts/model_rest.joblib")
+model_young = load("artifacts/model_young.joblib")
 
 
-scaler_rest = load("artifacts\scaler_rest.joblib")
-scaler_young = load("artifacts\scaler_young.joblib")
+scaler_rest = load("artifacts/scaler_rest.joblib")
+scaler_young = load("artifacts/scaler_young.joblib")
 
 
 def calculate_normalized_risk(medical_history):
